c28d6f91cad0191e2ea8efa8c77d8e109908c6b0
alfianrht <alvianrht@gmail.com>
2022-11-01 21:31:50 +0700

    Added Jejaring Profil

--- application/controllers/Jejaring.php
+++ application/controllers/Jejaring.php
@@ -8,4 +8,11 @@ class Jejaring extends CI_Controller {
 		$this->load->view('user/jejaring', $var);
 		$this->load->view('layout/user/footer', $var);
 	}
+
+	public function profil(){
+        $var['title'] = "Profil Jejaring";
+		$this->load->view('layout/user/header', $var);
+		$this->load->view('user/jejaring-profil', $var);
+		$this->load->view('layout/user/footer', $var);
+	}
 }
---
+++ application/views/user/jejaring-profil.php
@@ -0,0 +1,219 @@
+
+      <div class="profile-cover text-center">
+         <img class="img-fluid w-100" src="https://asset.kompas.com/crops/ScXltG26qzSypU8o2xMryodhDnM=/0x0:1000x667/750x500/data/photo/2020/01/29/5e30e9bc69af5.jpg" alt="" style="height: 250px; object-fit: cover;">
+      </div>
+      <div class="bg-white shadow-sm-bottom">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="d-flex align-items-center py-3">
+                     <div class="profile-left">
+                        <h5 class="font-weight-bold text-dark mb-1 mt-0">PT Wijaya Kusuma</h5>
+                        <p class="mb-0 text-muted"><a class="mr-2 font-weight-bold" href="#"> <i class="fa fa-map mr-1"></i> Singaparna, Kab. Tasikmalaya</a></p>
+                     </div>
+                     <div class="profile-right ml-auto">
+                        <button type="button" class="btn btn-light mr-1"> &nbsp; Lokasi &nbsp; </button>
+                        <button type="button" class="btn btn-success"> &nbsp; <i class="fa fa-whatsapp fa-lg mr-1" aria-hidden="true"></i> Hubungi Sekarang &nbsp; </button>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="py-4">
+         <div class="container">
+            <div class="row">
+               <!-- Main Content -->
+               <main class="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
+                  <div class="box shadow-sm rounded bg-white mb-3">
+                     <div class="box-title border-bottom p-3">
+                        <h6 class="m-0 font-weight-bold">Tentang Kami</h6>
+                     </div>
+                     <div class="box-body px-3 pt-2 pb-1">
+                        <p class="text-justify mb-2">Kami merupakan perusahan pengolahan bahan pangan, yang akan membantu para penyedia bahan agar bisa diolah untuk menjadi bahan yang lebih siap untuk digunakan</p>
+                        <p class="text-justify mb-2">Agar lebih memberikan kesempatan pada pelaku tani yang ada di sana, maka kami akan senang hati untuk berkolaborasi pada jejaring tanaman pangan ini. Menciptakan kolaborasi pangan yang sangat menjanjikan kedepannya.</p>
+                     </div>
+                  </div>
+                  <div class="box shadow-sm rounded bg-white mb-3">
+                     <div class="box-title border-bottom p-3">
+                        <h6 class="m-0 font-weight-bold">Biodata Lengkap</h6>
+                     </div>
+                     <div class="box-body">
+                        <table class="table table-borderless mb-0">
+                           <tbody>
+                              <tr class="border-bottom">
+                                 <th class="p-3" width="30%;">Lembaga</th>
+                                 <td class="p-3">PT Wijaya Kusuma</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">No HP</th>
+                                 <td class="p-3">082112904440</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Alamat</th>
+                                 <td class="p-3">Jl. Sukarame No. 11 RT/RW 02/03, Singaparna, Kab. Tasikmalaya, Jawa Barat, 91221</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Nama PJ</th>
+                                 <td class="p-3">Alfian Rahmatullah</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Komoditas</th>
+                                 <td class="p-3">Padi, Jagung, Sorgum, Jagung, Kedelai, Singkong</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Lahan</th>
+                                 <td class="p-3">35 Hektar</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Produksi</th>
+                                 <td class="p-3">30 Kg <sup class="text-danger">/tahun</sup></td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Kebutuhan</th>
+                                 <td class="p-3">10 Kg <sup class="text-danger">/bulan</sup></td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Produk Dijual</th>
+                                 <td class="p-3">10 Kg <sup class="text-danger">/bulan</sup></td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Produk Dibutuhkan</th>
+                                 <td class="p-3">10 Kg <sup class="text-danger">/bulan</sup></td>
+                              </tr>
+                           </tbody>
+                        </table>
+                     </div>
+                  </div>
+
+                  <div class="box shadow-sm rounded bg-white mb-3">
+                     <div class="box-title border-bottom p-3">
+                        <h6 class="m-0 font-weight-bold">Olahan</h6>
+                     </div>
+                     <div class="box-body">
+                        <table class="table table-borderless mb-0">
+                           <tbody>
+                              <tr class="border-bottom">
+                                 <th class="p-3" width="30%;">Kategori</th>
+                                 <td class="p-3">Makanan, Minuman, Pakan, Industri</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Jenis</th>
+                                 <td class="p-3">Roti, Makanan</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Produksi</th>
+                                 <td class="p-3">30 Kg <sup class="text-danger">/bulan</sup></td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Pupuk</th>
+                                 <td class="p-3">Kiloan</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Pestisida</th>
+                                 <td class="p-3">-</td>
+                              </tr>
+                              <tr class="border-bottom">
+                                 <th class="p-3">Alsintan</th>
+                                 <td class="p-3">-</td>
+                              </tr>
+                           </tbody>
+                        </table>
+                     </div>
+                  </div>
+               </main>
+               <aside class="col col-xl-3 order-xl-1 col-lg-6 order-lg-2 col-md-6 col-sm-6 col-12">
+                  <div class="box mb-3 shadow-sm rounded border-dark bg-white profile-box text-center">
+                     <div class="px-3 py-3">
+                        <span class="badge badge-warning mb-3 w-100 py-1 text-uppercase">Petani/Produsen Pangan</span>
+                        <img src="https://static.vecteezy.com/system/resources/thumbnails/007/684/509/small/alphabet-letters-initials-monogram-logo-tp-pt-t-and-p-free-vector.jpg" class="img-fluid border border-dark rounded-circle" style="height: 100px; width: 100px; object-fit: cover;" alt="Responsive image">
+                     </div>
+                     <div class="p-3 border-top border-bottom">
+                        <h6 class="font-weight-bold text-dark mb-2 mt-0">PT Wijaya Kusuma</h6>
+                        <p class="mb-0 text-muted">Jl. Sukarame No. 11 RT/RW 02/03, Singaparna, Kab. Tasikmalaya, Jawa Barat, 91221
+                        </p>
+                     </div>
+                     <div class="p-3">
+                        <div class="d-flex align-items-top mb-2">
+                           <p class="mb-0 text-muted">Whatsapp</p>
+                           <p class="font-weight-bold text-dark mb-0 mt-0 ml-auto">082112905550</p>
+                        </div>
+                        <div class="d-flex align-items-top mb-2">
+                           <p class="mb-0 text-muted">Facebook</p>
+                           <p class="font-weight-bold text-dark mb-0 mt-0 ml-auto">@ptwijayakusuma</p>
+                        </div>
+                        <div class="d-flex align-items-top mb-2">
+                           <p class="mb-0 text-muted">Youtube</p>
+                           <p class="font-weight-bold text-dark mb-0 mt-0 ml-auto">-</p>
+                        </div>
+                        <div class="d-flex align-items-top mb-2">
+                           <p class="mb-0 text-muted">Tiktok</p>
+                           <p class="font-weight-bold text-dark mb-0 mt-0 ml-auto">@ptwijayakusuma</p>
+                        </div>
+                     </div>
+                  </div>
+
+                  <div class="box shadow-sm">
+                    
+                  </div>
+                  
+                  <div class="box shadow-sm rounded bg-white mb-3">
+                     <div class="box-title border-bottom p-3 d-flex align-items-center">
+                        <h6 class="m-0">Galleri</h6>
+                        <a class="ml-auto text-info" href="#">Selengkapnya <i class="fa fa-arrow-right ml-2"></i></a>
+                     </div>
+                     <div class="box-body p-3">
+                        <p class="mb-2 font-weight-bold">Tampak Depan</p>
+                        <img class="img-fluid w-100 mb-1" src="https://asset.kompas.com/crops/ScXltG26qzSypU8o2xMryodhDnM=/0x0:1000x667/750x500/data/photo/2020/01/29/5e30e9bc69af5.jpg" alt="" style="object-fit: cover; height: 70px; border-radius: 10px;">
+                        <p class="mb-2 font-weight-bold">Foto Lainnya</p>
+                        <div class="gallery-box-main">
+                           <div class="gallery-box">
+                              <img class="img-fluid" src="https://asset.kompas.com/crops/ScXltG26qzSypU8o2xMryodhDnM=/0x0:1000x667/750x500/data/photo/2020/01/29/5e30e9bc69af5.jpg" alt="" style="object-fit: cover; height: 70px;">
+                              <img class="img-fluid" src="https://asset.kompas.com/crops/ScXltG26qzSypU8o2xMryodhDnM=/0x0:1000x667/750x500/data/photo/2020/01/29/5e30e9bc69af5.jpg" alt="" style="object-fit: cover; height: 70px;">
+                              <img class="img-fluid" src="https://asset.kompas.com/crops/ScXltG26qzSypU8o2xMryodhDnM=/0x0:1000x667/750x500/data/photo/2020/01/29/5e30e9bc69af5.jpg" alt="" style="object-fit: cover; height: 70px;">
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="box shadow-sm mb-3 rounded bg-white ads-box text-center">
+                     <img src="<?= base_url() ?>assets/images/job1.png" class="img-fluid" alt="Responsive image">
+                     <div class="p-3 border-bottom">
+                        <h6 class="font-weight-bold text-dark">Osahan Solutions</h6>
+                        <p class="mb-0 text-muted">Looking for talent?</p>
+                     </div>
+                     <div class="p-3">
+                        <button type="button" class="btn btn-outline-success pl-4 pr-4"> POST A JOB </button>
+                     </div>
+                  </div>
+               </aside>
+               <aside class="col col-xl-3 order-xl-3 col-lg-6 order-lg-3 col-md-6 col-sm-6 col-12">
+                  <div class="box shadow-sm rounded bg-white mb-3">
+                     <div class="box-title border-bottom p-3">
+                        <h6 class="m-0">Data Lainnya
+                        </h6>
+                     </div>
+                     <div class="box-body">
+                        <div class="linked-accounts px-3 py-2 border-bottom">
+                           <h6 class="font-weight-bold">Penjualan</h6>
+                           <ul>
+                              <li class="mb-1"><i class="fa fa-check-circle mr-2 text-info" aria-hidden="true"></i><span class="text">Dalam Kab/Kota</span></li>
+                              <li class="mb-1"><i class="fa fa-check-circle mr-2 text-info" aria-hidden="true"></i><span class="text">Antar Kab/Kota</span></li>
+                           </ul>
+                        </div>
+                        <div class="linked-accounts px-3 py-2">
+                           <h6 class="font-weight-bold">Tertarik</h6>
+                           <ul>
+                              <li class="mb-1"><i class="fa fa-check-circle mr-2 text-info" aria-hidden="true"></i><span class="text">Jual Produk Segar</span></li>
+                              <li class="mb-1"><i class="fa fa-check-circle mr-2 text-info" aria-hidden="true"></i><span class="text">Jual Produk Olahan</span></li>
+                              <li class="mb-1"><i class="fa fa-check-circle mr-2 text-info" aria-hidden="true"></i><span class="text">Jual Saprodi</span></li>
+                              <li class="mb-1"><i class="fa fa-check-circle mr-2 text-info" aria-hidden="true"></i><span class="text">Beli Produk Segar</span></li>
+                              <li class="mb-1"><i class="fa fa-check-circle mr-2 text-info" aria-hidden="true"></i><span class="text">Beli Produk Olahan</span></li>
+                              <li class="mb-1"><i class="fa fa-check-circle mr-2 text-info" aria-hidden="true"></i><span class="text">Beli Saprodi</span></li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </aside>
+            </div>
+         </div>
+      </div>
