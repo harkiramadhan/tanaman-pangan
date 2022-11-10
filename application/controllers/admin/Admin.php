d7f47394fecbf1ff20ec879a87505a9872e181d6
alfianrht <alvianrht@gmail.com>
2022-11-11 02:45:08 +0700

    Dashboard Admin Tanaman Pangan

---
+++ application/controllers/admin/Admin.php
@@ -0,0 +1,10 @@
+<?php
+
+class Admin extends CI_Controller {
+	public function index(){
+        $var['title'] = "Beranda Admin";
+		// $this->load->view('layout/admin/header', $var);
+		$this->load->view('admin/beranda', $var);
+		// $this->load->view('layout/admin/footer', $var);
+	}
+}
