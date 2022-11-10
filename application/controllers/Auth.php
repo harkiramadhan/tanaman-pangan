6685cbae75b0eabaa25fb0f15e3af089902ded21
harkiramadhan <harkiramadhan@gmail.com>
2022-11-10 18:21:41 +0700

    updates

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -30,6 +30,13 @@ class Auth extends CI_Controller {
 
 	/* Action Here */
 	function register(){
+		$dataInsert = [
+			'nama' => $this->input->post('nama', TRUE),
+			'nohp' => $this->input->post('nohp', TRUE),
+			'password' => $this->input->post('password', TRUE),
+			'role_id' => $this->input->post('role_id', TRUE),
+		];
+		
 		$this->output->set_content_type('application/json')->set_output(json_encode($this->input->post()));
 	}
 }
